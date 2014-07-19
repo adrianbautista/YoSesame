96aac4b7cd586b6f146da5c15d91fdaf5577f539
Adrian Bautista <adrianbautista8@gmail.com>
2014-07-19 13:52:39 -0400

    Add basic sign in form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def edit
@@ -12,8 +19,15 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :yo_username)
   end
 end
