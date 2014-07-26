7fbabbc58e4e1b24f700f733e02905d08d86109d
Adrian Bautista <adrianbautista8@gmail.com>
2014-07-26 18:23:49 -0400

    Refactor yo message to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, :yo_username, presence: true, uniqueness: true
 
+  YO_API_ENDPOINT = 'http://api.justyo.com'
+
   def self.find_and_authenticate(email, password)
     find_by(email: email).try(:authenticate, password)
+  end
+
+  def send_yo_link(link)
+    RestClient.post(YO_API_ENDPOINT, api_token: ENV['YO_API_KEY'], username: yo_username, link: link)
   end
 end
