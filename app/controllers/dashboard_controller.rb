a2feed4410e6a213ca882255012e4509e3f237c5
Dustin Coates <dustin@dcoates.com>
2014-07-26 20:42:08 -0400

    Only show dashboard if logged in

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    # @user = User.find(session[:user_id])
-    # Yeah, yeah, I know
+    @user = User.where(id: session[:user_id]).first
+    if !@user || !@user.yo_confirmed
+      redirect_to root_path
+    end
   end
 end
